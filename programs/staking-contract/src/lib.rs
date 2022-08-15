2b364654af1280f0f85fb1d03a857c1da2e20e92
asmitbhantana <bhantanasmit@gmail.com>
2022-08-16 01:18:31 +0545

    first commit

---
+++ programs/staking-contract/src/lib.rs
@@ -0,0 +1,170 @@
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken, 
+
+    token::{ Token, Transfer, TokenAccount, Mint}
+};
+use anchor_lang::require;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod staking_contract {
+    use super::*;
+
+    pub fn perform_action(
+        ctx: Context<PerformAction>,
+        action_amount: u64, 
+        action_token: Pubkey,
+        stake_action: bool
+    ) -> Result<()> {
+       let current_user = ctx.accounts.staker.clone();
+       let token_program = ctx.accounts.token_mint.clone();
+    
+       let token_program_key = ctx.accounts.token_program.clone().key();
+       let current_staking_pool_account = ctx.accounts.current_staking_pool.clone().to_account_info();
+       let staking_pool = &mut ctx.accounts.current_staking_pool;
+       let pool_action = &mut ctx.accounts.pool_action;
+
+       require!(token_program.key() == action_token, ErrorCode::InvalidToken);  
+
+       if stake_action {
+
+        pool_action.staker = current_user.key();
+        pool_action.token_amount = action_amount;
+        pool_action.mint_start_time = 7u64; //For testing
+
+        let transfer_instruction = Transfer{
+            from: ctx.accounts.staker_associated_address.to_account_info(),
+            to: ctx.accounts.staking_vault_associated_address.to_account_info(),
+            authority: ctx.accounts.staker.to_account_info(),
+        };
+        let cpi_ctx = CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            transfer_instruction,
+        );
+        anchor_spl::token::transfer(cpi_ctx, action_amount)?;
+
+        // pool_action.staking_action = true;
+        staking_pool.token_amount += action_amount;
+       }
+
+       else {
+        
+        require!(pool_action.staker == ctx.accounts.staker.key(), ErrorCode::InvalidToken);
+        require!(pool_action.token_amount <= action_amount, ErrorCode::NotEnoughToken);
+        require!(pool_action.staking_action , ErrorCode::InvalidPoolAction);
+
+        pool_action.token_amount -= action_amount;
+
+         let bump_seed_staking_pool = ctx.bumps.get("current_staking_pool").unwrap().to_le_bytes();
+         let staking_pool_signer_seeds: &[&[_]] = &[
+            b"stake_pool".as_ref(),
+            &token_program_key.as_ref(),
+            &bump_seed_staking_pool
+        ];
+
+        let transfer_instruction = Transfer{
+            from: ctx.accounts.staking_vault_associated_address.to_account_info(),
+            to: ctx.accounts.staker_associated_address.to_account_info(),
+            authority: current_staking_pool_account.clone(),
+        };
+        let signer = &[staking_pool_signer_seeds];
+        let cpi_ctx = CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            transfer_instruction,
+            signer,
+            );
+        anchor_spl::token::transfer(cpi_ctx, action_amount)?;
+
+        pool_action.staking_action = false;
+        staking_pool.token_amount -= action_amount;
+
+       }
+       Ok(())
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(action_amount: u64, action_token: Pubkey, stake_action: bool)]
+pub struct PerformAction<'info> {
+    #[account(mut)]
+    staker: Signer<'info>, 
+
+    #[account(
+        init_if_needed,
+        payer = staker,
+        space = 8 + 32 + 8,
+        seeds = [
+            b"stake_pool".as_ref(),
+            // token_mint.key().as_ref()
+        ],
+        bump
+    )]
+    current_staking_pool: Account<'info, StakePool>,
+
+    #[account(
+        init_if_needed,
+        payer = staker, 
+        space = 8 + 32 + 8 + 8 + 1
+    )]
+    pool_action: Account<'info, PoolAction>,
+
+    #[account(
+        init_if_needed,
+        payer = staker,
+        associated_token::mint = token_mint,
+        associated_token::authority = current_staking_pool,
+    )]
+    staking_vault_associated_address: Box<Account<'info, TokenAccount>>,
+
+    #[account(
+         mut,
+        constraint= staker_associated_address.owner == staker.key(),
+        constraint= staker_associated_address.mint == token_mint.key(),
+        constraint= staker_associated_address.amount >= action_amount
+    )]
+    staker_associated_address: Box<Account<'info, TokenAccount>>,
+
+    token_mint: Account<'info, Mint>,
+
+    associated_token_program: Program<'info, AssociatedToken>,
+    system_program: Program<'info, System>,
+    token_program: Program<'info, Token>,
+    rent: Sysvar<'info, Rent>
+
+}
+
+#[account]
+#[derive(Default)]
+pub struct StakePool {
+    token_mint: Pubkey,
+    token_amount: u64
+}
+
+#[account]
+#[derive(Default)]
+pub struct PoolAction{
+    staker: Pubkey,
+    token_amount: u64,
+    mint_start_time: u64,
+    staking_action: bool,
+}
+
+
+#[error_code]
+pub enum ErrorCode {
+
+    #[msg("Not Enough Token")]
+    NotEnoughToken,
+
+    #[msg("Not Valid Token")]
+    InvalidToken, 
+
+    #[msg("Not Valid User")]
+    InvalidUser, 
+
+    #[msg("Not Valid Pool Action Acccount Provided")]
+    InvalidPoolAction, 
+    
+}
---
+++ tests/staking-contract.ts
@@ -0,0 +1,206 @@
+import * as anchor from '@project-serum/anchor';
+import { Program } from '@project-serum/anchor';
+import { StakingContract } from '../target/types/staking_contract';
+
+import * as spl from '@solana/spl-token';
+import { assert, expect } from 'chai';
+
+interface PDAParameters {
+  stake_pool: anchor.web3.PublicKey;
+}
+
+describe('Stake', () => {
+  const provider = anchor.AnchorProvider.local();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.StakingContract as Program<StakingContract>;
+
+  let mintAddress: anchor.web3.PublicKey;
+
+  let alice: anchor.web3.Keypair;
+  let aliceTokenAccount: anchor.web3.PublicKey;
+
+  let bob: anchor.web3.Keypair;
+  let bobTokenAccount: anchor.web3.PublicKey;
+
+  let pda: PDAParameters;
+
+  //get PDA of Stake Pool
+  const getPdaParams = async (
+    token_program: anchor.web3.PublicKey
+  ): Promise<PDAParameters> => {
+    let [stake_pool, stake_bump] =
+      await anchor.web3.PublicKey.findProgramAddress(
+        [Buffer.from('stake_pool')],
+        program.programId
+      );
+
+    return {
+      stake_pool: stake_pool,
+    };
+  };
+
+  //Create a SPL Token
+  const createMint = async (): Promise<anchor.web3.PublicKey> => {
+    const tokenMint = new anchor.web3.Keypair();
+    const lamportsForMint =
+      await provider.connection.getMinimumBalanceForRentExemption(
+        spl.MintLayout.span
+      );
+    let tx = new anchor.web3.Transaction();
+
+    // Allocate mint
+    tx.add(
+      anchor.web3.SystemProgram.createAccount({
+        programId: spl.TOKEN_PROGRAM_ID,
+        space: spl.MintLayout.span,
+        fromPubkey: provider.wallet.publicKey,
+        newAccountPubkey: tokenMint.publicKey,
+        lamports: lamportsForMint,
+      })
+    );
+    // Allocate wallet account
+    tx.add(
+      spl.createInitializeMintInstruction(
+        tokenMint.publicKey,
+        6,
+        provider.wallet.publicKey,
+        provider.wallet.publicKey,
+        spl.TOKEN_PROGRAM_ID
+      )
+    );
+    const signature = await provider.sendAndConfirm(tx, [tokenMint]);
+
+    // console.log(`[${tokenMint.publicKey}] Created new mint account at ${signature}`);
+    return tokenMint.publicKey;
+  };
+
+  //Create a User Associated Wallet for SPL Tokens
+  const createUserAndAssociatedWallet = async (
+    mint: anchor.web3.PublicKey,
+    amount: number
+  ): Promise<[anchor.web3.Keypair, anchor.web3.PublicKey | undefined]> => {
+    const user = new anchor.web3.Keypair();
+    let userAssociatedTokenAccount: anchor.web3.PublicKey | undefined =
+      undefined;
+
+    // Fund user with some SOL
+    let txFund = new anchor.web3.Transaction();
+    txFund.add(
+      anchor.web3.SystemProgram.transfer({
+        fromPubkey: provider.wallet.publicKey,
+        toPubkey: user.publicKey,
+        lamports: 5 * anchor.web3.LAMPORTS_PER_SOL,
+      })
+    );
+    const sigTxFund = await provider.sendAndConfirm(txFund);
+    //console.log(`[${user.publicKey.toBase58()}] Funded new account with 5 SOL: ${sigTxFund}`);
+
+    if (mint) {
+      // Create a token account for the user and mint some tokens
+      userAssociatedTokenAccount = await spl.getAssociatedTokenAddress(
+        mint,
+        user.publicKey,
+        false,
+        spl.TOKEN_PROGRAM_ID,
+        spl.ASSOCIATED_TOKEN_PROGRAM_ID
+      );
+      const txFundTokenAccount = new anchor.web3.Transaction();
+      txFundTokenAccount.add(
+        spl.createAssociatedTokenAccountInstruction(
+          user.publicKey,
+          userAssociatedTokenAccount,
+          user.publicKey,
+          mint,
+          spl.TOKEN_PROGRAM_ID,
+          spl.ASSOCIATED_TOKEN_PROGRAM_ID
+        )
+      );
+      txFundTokenAccount.add(
+        spl.createMintToInstruction(
+          mint,
+          userAssociatedTokenAccount,
+          provider.wallet.publicKey,
+          amount,
+          [],
+          spl.TOKEN_PROGRAM_ID
+        )
+      );
+      const txFundTokenSig = await provider.sendAndConfirm(txFundTokenAccount, [
+        user,
+      ]);
+      // console.log(`[${userAssociatedTokenAccount.toBase58()}] New associated account for mint ${mint.toBase58()}: ${txFundTokenSig}`);
+    }
+    return [user, userAssociatedTokenAccount];
+  };
+
+  const readAccount = async (
+    accountPublicKey: anchor.web3.PublicKey
+  ): Promise<[string]> => {
+    const tokenInfoLol = await provider.connection.getAccountInfo(
+      accountPublicKey
+    );
+    const data = Buffer.from(tokenInfoLol.data);
+    const accountInfo: spl.AccountInfo = spl.AccountLayout.decode(data);
+    return [accountInfo.amount.toString()];
+  };
+
+  before(async () => {
+    //c8 mint token
+    mintAddress = await createMint();
+    //transfer token to alice
+    [alice, aliceTokenAccount] = await createUserAndAssociatedWallet(
+      mintAddress,
+      20000000
+    );
+    //init token account for bob
+    [bob, bobTokenAccount] = await createUserAndAssociatedWallet(
+      mintAddress,
+      0
+    );
+    // //c8 program state account and program's token account
+    pda = await getPdaParams(mintAddress);
+    //check before calling program
+    let aliceBalance = await readAccount(aliceTokenAccount);
+    assert.equal(aliceBalance, '20000000');
+    let bobBalance = await readAccount(bobTokenAccount);
+    assert.equal(bobBalance, '0');
+  });
+
+  it('Staking', async () => {
+    let staking_amount = '20000000';
+    let staking_token = mintAddress;
+    let stake_action = true;
+    let pool_action_account = new anchor.web3.Keypair();
+    let stakingVaultAssociatedAddress = await spl.getAssociatedTokenAddress(
+      mintAddress,
+      pda.stake_pool,
+      true,
+      spl.TOKEN_PROGRAM_ID,
+      spl.ASSOCIATED_TOKEN_PROGRAM_ID
+    );
+    await program.rpc.performAction(
+      new anchor.BN(staking_amount),
+      staking_token,
+      stake_action,
+      {
+        accounts: {
+          staker: alice.publicKey,
+          tokenMint: mintAddress,
+          currentStakingPool: pda.stake_pool,
+          poolAction: pool_action_account.publicKey,
+          stakerAssociatedAddress: aliceTokenAccount,
+          stakingVaultAssociatedAddress: stakingVaultAssociatedAddress,
+          associatedTokenProgram: spl.ASSOCIATED_TOKEN_PROGRAM_ID,
+          tokenProgram: spl.TOKEN_PROGRAM_ID,
+          systemProgram: anchor.web3.SystemProgram.programId,
+          rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+        },
+        // signers: [alice],
+        signers: [alice, pool_action_account],
+      }
+    );
+    let aliceBalance = await readAccount(aliceTokenAccount);
+    assert.equal(aliceBalance, '0');
+  });
+});
